735630456e4c1d6d12432d80cab231a0d0e7f2b8
Rishika Sankaran <rishikasankaran@gmail.com>
2023-01-13 17:22:49 +0530

    Adjusting alignment - Portfolio 1.0

--- pages/education.tsx
+++ pages/education.tsx
@@ -7,11 +7,11 @@ export default function Education() {
       <h2 className={edu_styles.titles}>EDUCATION</h2>
       <p className={edu_styles.uni} >University of California, Irvine</p>
       <p className={edu_styles.dates}>Dec 2022</p>
-      <p className = {edu_styles.uni}>Master of Computer Science, GPA : 3.83/4.0 </p>
+      <p>Master of Computer Science, GPA : 3.83/4.0 </p>
 
       <p className={edu_styles.uni}>Visvesvaraya Technological University, Bangalore, India </p>
       <p className={edu_styles.dates}>2017 - 2021</p>
-      <p className = {edu_styles.uni}>BE in Computer Science Engineering, GPA: 3.7/4.0</p>
+      <p>BE in Computer Science Engineering, GPA: 3.7/4.0</p>
   </>
   )
 }
