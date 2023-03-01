4e9eccbbe2533b95008f33fd257692ea0f1e867b
Rishika Sankaran <rishikasankaran@gmail.com>
2023-02-28 20:01:29 -0800

    Adding skills

---
+++ components/skillsTable.tsx
@@ -0,0 +1,32 @@
+import skillsTableStyles from '../styles/components/skillsTable.module.css'
+
+export default function SkillsTable() {
+  return (
+  <div >
+    <table className={skillsTableStyles.container}>
+    <tbody>
+        <tr>
+            <th scope="row" width='40%'>LANGUAGES / DATABASES</th>
+            <td>Python, Kotlin, PostgreSQL, MySQL, TypeScript</td>
+        </tr>
+        <tr>
+            <th scope="row" >FRAMEWORKS / LIBRARIES</th>
+            <td className={skillsTableStyles.alternateRow}>Git, NextJS, React, Pandas, Numpy, OpenCV, Keras</td>
+        </tr>
+        <tr>
+            <th scope="row">CONCEPTS</th>
+            <td>Machine Learning, Web Development, Deep Learning, Data Science</td>
+        </tr>
+        <tr>
+            <th scope="row">CERTIFIED SPECIALIZATIONS</th>
+            <td>Python, Deep Learning, Tensorflow Developer, Data Science</td>
+        </tr>
+        <tr>
+            <th scope="row">SELF-DIRECTED COURSES</th>
+            <td>Kotlin, Unity, AWS, Machine Learning</td>
+        </tr>
+        </tbody>
+    </table>
+  </div>
+  );
+}
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,12 +1,13 @@
 import styles from '../styles/Home.module.css'
 import project_styles from '../styles/projects.module.css'
 import Link from 'next/link'
+import SkillsTable from '../components/skillsTable'
 
 export default function Skills() {
   return (
   <>
       <h2 className={project_styles.titles}>SKILLS</h2>
-      <p> To be released soon. Deployment in progress. </p>
+      <SkillsTable/>
   </>
   )
 }
